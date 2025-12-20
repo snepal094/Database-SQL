fdd542a3c69de9cf4bd855606ad821e8b31bfbf6
Suyasha Nepal <108968670+snepal094@users.noreply.github.com>
2025-12-20 11:33:09 +0545

    used HR tables

--- Advanced Database/Advanced 5- LISTAGG.sql
+++ Advanced Database/Advanced 5- LISTAGG.sql
@@ -1,29 +1,10 @@
-SELECT * FROM employees00;
-
 SELECT department_id,
 LISTAGG(
-    firstname || ' ' || lastname, ', '
+    first_name || ' ' || last_name, ', '
 )
-WITHIN GROUP (ORDER BY lastname ASC) AS emp_names
-FROM employees00
+WITHIN GROUP (ORDER BY last_name ASC) AS emp_names
+FROM hr.employees
 GROUP BY department_id;
-
-ALTER TABLE employees00 DROP COLUMN department_name;
-
-CREATE TABLE departments (
-    department_id INT,
-    department_name VARCHAR2(50)
-);
-
-INSERT INTO departments VALUES
-(10, 'HR'),
-(20, 'IT'),
-(30, 'Finance'),
-(40, 'Marketing'),
-(50, 'Admin');
-
-SELECT * FROM employees00;
-SELECT * FROM departments;
 
 SELECT emp.department_id, dept.department_name,
 LISTAGG(
